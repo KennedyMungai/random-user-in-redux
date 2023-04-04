0a485aea3053e794e3931a6660803382e6cee281
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-04 12:46:55 +0300

    Imported users api

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { usersApi } from "../services/users";
+
 
 
 export const store = configureStore(
     {
         reducer: {}
-
     }
 )
