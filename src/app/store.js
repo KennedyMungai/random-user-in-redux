30bb007ee31c00835f4aaccaa4c6b1b1e2adb2d7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-04 12:47:26 +0300

    IMported setupListeners

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { usersApi } from "../services/users";
-
+import { setupListeners } from "@reduxjs/toolkit/dist/query";
 
 
 export const store = configureStore(
