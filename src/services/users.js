daf6581fa70eaffdc2e238bfc85bdd29fcec1f64
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-04 12:42:27 +0300

    Created the users API

--- src/services/users.js
+++ src/services/users.js
@@ -1,1 +1,18 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+
+export const usersApi = () => createApi({
+    reducerPath: "users",
+    baseQuery: fetchBaseQuery({
+        baseUrl: "https://randomuser.me"
+    }),
+    endpoints: (builder) => (
+        {
+            getUsers: builder.query(
+                {
+                    query: () => "api"
+                }
+            )
+        }
+    )
+})
